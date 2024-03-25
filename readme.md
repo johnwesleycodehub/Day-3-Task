68853397d07d6efef1d59486cec1101a279dc86f
johnwesleycodehub <johnwesley2307r@gmail.com>
2024-03-25 14:00:51 +0530

    Updated

---
+++ readme.md
@@ -0,0 +1,6 @@
+This is Day 3 task. Here I learnt about Object & array of objects, JSON, stringify, api, xmlhttprequest and solved some practice coding.
+Task 1 compare two JSON objects without considering the order and without using sorting.
+Task 2 Display all the country flags from the api
+Task 3 Display countryname,region,subregion,population from the api
+
+Day 3 task completed.
---
+++ script.js
@@ -0,0 +1,47 @@
+//! Task 1 compare two JSON objects without considering the order and without using sorting
+let obj1 = { name: "person 1", age: 5 };
+let obj2 = { age: 5, name: "person 1" };
+
+let json1 = JSON.stringify(obj1);
+let json2 = JSON.stringify(obj2);
+if (json1 === json2) {
+  let parsedObj1 = JSON.parse(json1);
+  let parsedObj2 = JSON.parse(json2);
+  if (
+    parsedObj1.name === parsedObj2.name &&
+    parsedObj1.age === parsedObj2.age
+  ) {
+    console.log("The name and age are equal.");
+  }
+} else {
+  console.log("The name and age are not equal.");
+}
+
+//! Task 2 Display all the country flags from the api
+var request = new XMLHttpRequest();
+request.open("GET", "https://restcountries.com/v3.1/all", true);
+request.send();
+request.onload = function () {
+  var data = request.response;
+  var result = JSON.parse(data);
+  for (var i = 0; i < result.length; i = i + 1) {
+    console.log(result[i].flags.png);
+  }
+};
+
+//! Task 3 Display countryname,region,subregion,population from the api
+var request1 = new XMLHttpRequest();
+request1.open("GET", "https://restcountries.com/v3.1/all", true);
+request1.send();
+request1.onload = function () {
+  var data1 = request1.response;
+  var result1 = JSON.parse(data1);
+  for (var i = 0; i < result1.length; i = i + 1) {
+    console.log(
+      "Countryname:" + " " + result1[i].name.common,
+      "Region:" + " " + result1[i].region,
+      "Subregion:" + " " + result1[i].subregion,
+      "Population:" + "" + result1[i].population
+    );
+  }
+};
